913c1b2d4716f91e7e17b8ccd309c03e3aeef664
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-04-23 20:40:15 -0300

    Agregando ejercicio de imc que utiliza RequestParam

---
+++ imccalc/src/main/java/com/IMCcalc/imccalc/ImcController/ImcController.java
@@ -0,0 +1,39 @@
+/*
+Api que al ingrar el IMC de una persona devuelva el estado de su peso
+rangos:
+1 a 18.5: peso insuficiente
+18.6 a 24.9: peso normal
+25 a 29.9: sobrepeso
+de 30 en adelante: obesidad
+usar @RequestParam
+*/
+
+package com.IMCcalc.imccalc.ImcController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Caro
+ */
+
+@RestController
+public class ImcController {
+    
+    @GetMapping("/imc")
+    public String imcRank(@RequestParam double valor){
+        if(valor>0 && valor<18.6){
+            return "Peso insuficiente (1 a 18.5): " + valor;
+        }else if(valor>18.5 && valor<25){
+                 return "Peso normal (18.6 a 24.9): " + valor;
+             }else if(valor>=25 && valor<29.9){
+                 return "Sobrepeso (25 a 29.9): " + valor;
+             }else {
+                 return "Obesidad (30 o +): " + valor;
+             }
+        
+    }
+    
+}
