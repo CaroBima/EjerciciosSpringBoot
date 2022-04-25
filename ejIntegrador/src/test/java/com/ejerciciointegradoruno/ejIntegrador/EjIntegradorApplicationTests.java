6ff3c8551b6b4e0a34b2b1fc14e3c625efccdd6d
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-04-24 21:46:01 -0300

    Iniciando el primer trabajo práctico integrador, cargando el proyecto creado en initializr con el enunciado del trabajo

---
+++ ejIntegrador/src/test/java/com/ejerciciointegradoruno/ejIntegrador/EjIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejerciciointegradoruno.ejIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
