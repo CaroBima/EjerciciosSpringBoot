fdf7506f0d859e8f4405b54f6648fe2865e76783
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-04-25 05:15:41 -0300

    agregando resolución del trabajo práctico integrador

---
+++ ejIntegrador/src/main/java/com/ejerciciointegradoruno/ejIntegrador/Controller/ConversorController.java
@@ -0,0 +1,31 @@
+/*
+Un turista norteamericano decidió recorrer en auto diferentes paises de América Latina.
+A la hora de cargar combustible, para su sorpresa, se encontró con que en la mayor
+parte de los países no se utiliza la medida "galones" sino que se utiliza "litros" a
+la hora de reabastecerse. 
+Para poder solicitar correctamente la cantidad de litros que necesita, requiere el
+desarrollo de una API que, al enviarle la cantidad de galones, otorgue como respuesta
+el equivalente en litros. 
+Para el desarrollo tener en cuenta que 1 galon es equivalente a 3.78541 litros. 
+*/
+
+
+package com.ejerciciointegradoruno.ejIntegrador.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Caro
+ */
+@RestController
+public class ConversorController {
+    
+    @GetMapping("/conversor")
+    public String conversorgalones(@RequestParam double galones){
+        double litros = galones *3.78541;
+        return galones + " galones equivalen a " + litros + " litros.";
+    }
+}
