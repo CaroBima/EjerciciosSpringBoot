00b01fcb683716bfc2976a8a8a1cf44963a15b17
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-05-18 13:02:38 -0300

    modificando nombres de archivos y comenzando ejercicio de modelo de capas

---
+++ modeloCapas/src/main/java/com/modelocapas/modeloCapas/ModeloCapasApplication.java
@@ -0,0 +1,13 @@
+package com.modelocapas.modeloCapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloCapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloCapasApplication.class, args);
+	}
+
+}
---
+++ modeloCapas/src/test/java/com/modelocapas/modeloCapas/ModeloCapasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelocapas.modeloCapas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloCapasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
