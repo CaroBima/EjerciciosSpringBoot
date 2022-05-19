69180c960f3d4ecffbac8ac784dc64bf30e5e6d3
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-05-18 21:10:45 -0300

    agregando interfaz del repo

---
+++ 09 - modeloCapas/src/main/java/com/modelocapas/modeloCapas/repository/IPersonaRepository.java
@@ -0,0 +1,12 @@
+
+package com.modelocapas.modeloCapas.repository;
+
+import com.modelocapas.modeloCapas.model.Persona;
+import java.util.List;
+
+public interface IPersonaRepository {
+    
+    public void guardarPersona(Persona persona);
+    public List traerListaPersonas();
+    
+}
--- 09 - modeloCapas/src/main/java/com/modelocapas/modeloCapas/repository/PersonaRepository.java
+++ 09 - modeloCapas/src/main/java/com/modelocapas/modeloCapas/repository/PersonaRepository.java
@@ -1,10 +1,22 @@
 
 package com.modelocapas.modeloCapas.repository;
 
+import com.modelocapas.modeloCapas.model.Persona;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public class PersonaRepository {
+public class PersonaRepository implements IPersonaRepository {
+
+    @Override
+    public void guardarPersona(Persona persona) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List traerListaPersonas() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     //metodos para llamar a la base de datos
 }
