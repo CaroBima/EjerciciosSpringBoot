e59068f68b533bb6d57bd887de71b4a86c2ff698
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-05-21 16:39:04 -0300

    modifiando los nombres de las clases

--- 10 - InyeccionDependenciasConstructor/src/main/java/com/mycompany/inyecciondependenciasconstructor/ServicioLavado.java
+++ 10 - InyeccionDependenciasConstructor/src/main/java/com/mycompany/inyecciondependenciasconstructor/ServicioLavado.java
@@ -4,22 +4,22 @@ package com.mycompany.inyecciondependenciasconstructor;
 
 
 
-public class Lavadero {
-    private LavadoNormal lavNorm;
-    private LavadoPremium lavPrem;
+public class ServicioLavado {
+    private ServicioNormal lavNorm;
+    private ServicioPremium lavPrem;
 
     //inyeccion de dependencias por constructo
-    public Lavadero(LavadoNormal lavNorm, LavadoPremium lavPrem) {
+    public ServicioLavado(ServicioNormal lavNorm, ServicioPremium lavPrem) {
         this.lavNorm = lavNorm;
         this.lavPrem = lavPrem;
     }
     
     //inyeccion de dependencias por setters
-    public void setLavNorm(LavadoNormal lavNorm) {
+    public void setLavNorm(ServicioNormal lavNorm) {
         this.lavNorm = lavNorm;
     }
 
-    public void setLavPrem(LavadoPremium lavPrem) {
+    public void setLavPrem(ServicioPremium lavPrem) {
         this.lavPrem = lavPrem;
     }
     
---
+++ 10 - InyeccionDependenciasConstructor/src/main/java/com/mycompany/inyecciondependenciasconstructor/ServicioPremium.java
@@ -0,0 +1,11 @@
+
+
+package com.mycompany.inyecciondependenciasconstructor;
+
+/**
+ *
+ * @author Caro
+ */
+public class ServicioPremium {
+    
+}
