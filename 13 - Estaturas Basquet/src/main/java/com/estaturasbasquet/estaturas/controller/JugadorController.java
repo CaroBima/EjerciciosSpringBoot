580af6407e0b56794963bcb88e4448aef7ea3022
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-05-25 22:17:38 -0300

    Terminado el endpoint que calcula el promedio de alturas y lo muestra

--- 13 - Estaturas Basquet/src/main/java/com/estaturasbasquet/estaturas/controller/JugadorController.java
+++ 13 - Estaturas Basquet/src/main/java/com/estaturasbasquet/estaturas/controller/JugadorController.java
@@ -5,6 +5,7 @@ package com.estaturasbasquet.estaturas.controller;
 import com.estaturasbasquet.estaturas.model.Jugador;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,18 +19,32 @@ public class JugadorController {
     
    List<Jugador> listaJugadores = new ArrayList(); 
     
-    @PostMapping("/nuevo/jugador")
+   //endpoint que permite agregar un nuevo jugador al arraylist 
+   @PostMapping("/nuevo/jugador")
     public void guardarJugador(@RequestBody Jugador jugador){
-        
-        
-        
-        System.out.println("antes del array");       
+
+        //Agrego el jugador en el array y lo recorro para mostrar por consola, para ver si funciona bien
         listaJugadores.add(jugador);
         
-        System.out.println("antes del for");       
+//        for (Jugador jug : listaJugadores){
+//            System.out.println("jugador " + jug.getApellido());
+//            System.out.println("jugador " + jug.getEstatura());
+//        }
+        
+    }
+
+    //endpoint para mostrar el promedio de estatura de los jugadores
+    @GetMapping("/promedio")
+    public double calcularPromedio(){
+        int sumatoriaEstaturas = 0;
+        
+        
+        //recorro el array y hago la sumatoria de estaturas
         for (Jugador jug : listaJugadores){
-            System.out.println("jugador " + jug.getApellido());
+            sumatoriaEstaturas += jug.getEstatura();
+            
         }
         
+        return sumatoriaEstaturas / listaJugadores.size();
     }
 }
