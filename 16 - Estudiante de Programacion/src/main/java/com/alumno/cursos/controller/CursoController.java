4a434c6dc44c1e4c7a5375e36fe21faf056f656f
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-06-07 20:31:21 -0300

    Comenzando con la capa controller

---
+++ 16 - Estudiante de Programacion/src/main/java/com/alumno/cursos/controller/CursoController.java
@@ -0,0 +1,18 @@
+
+
+package com.alumno.cursos.controller;
+
+import com.alumno.cursos.repository.ICursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Caro
+ */
+@RestController
+public class CursoController {
+    @Autowired
+    ICursoRepository cursoRepo;
+    
+}
