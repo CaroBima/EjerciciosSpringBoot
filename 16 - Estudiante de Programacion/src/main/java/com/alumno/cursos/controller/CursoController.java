7ea7a7183304fc52fa0da1e7901f1bf1aa897457
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-06-08 00:07:21 -0300

    controller

--- 16 - Estudiante de Programacion/src/main/java/com/alumno/cursos/controller/CursoController.java
+++ 16 - Estudiante de Programacion/src/main/java/com/alumno/cursos/controller/CursoController.java
@@ -15,4 +15,6 @@ public class CursoController {
     @Autowired
     ICursoRepository cursoRepo;
     
+    
+    
 }
--- 16 - Estudiante de Programacion/src/main/java/com/alumno/cursos/controller/TemaController.java
+++ 16 - Estudiante de Programacion/src/main/java/com/alumno/cursos/controller/TemaController.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.alumno.cursos.controller;
+
+import com.alumno.cursos.repository.ITemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Caro
  */
+@RestController
 public class TemaController {
-    
+    @Autowired
+    ITemaRepository temaRepo;
 }
